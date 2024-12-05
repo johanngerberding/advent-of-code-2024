d595d94a03ccd970dbc2208ed190293138bf0bd8
johanngerberding <johann.gerberding@lector.ai>
2024-12-05 15:29:30 +0100

    day 5 part 2

--- problems/day05/main.py
+++ problems/day05/main.py
@@ -1,3 +1,5 @@
+import functools
+
 test = """47|53
 97|13
 97|61
@@ -28,8 +30,8 @@ test = """47|53
 97,13,75,29,47"""
 
 test = test.split()
-rules = [[int(n) for n in el.split("|")] for el in test if "|" in el]
-updates = [[int(n) for n in el.split(",")] for el in test if "|" not in el]
+test_rules = [[int(n) for n in el.split("|")] for el in test if "|" in el]
+test_updates = [[int(n) for n in el.split(",")] for el in test if "|" not in el]
 
 
 def solve(rules: list, updates: list, part: str):
@@ -67,7 +69,7 @@ def solve(rules: list, updates: list, part: str):
     print(f"{part}: {middle_sum}")
 
 
-solve(rules, updates, "Test")
+solve(test_rules, test_updates, "Test")
 
 
 with open("input.txt", "r") as fp:
@@ -79,3 +81,48 @@ rules = [[int(n) for n in el.split("|")] for el in data if "|" in el]
 updates = [[int(n) for n in el.split(",")] for el in data if "|" not in el]
 
 solve(rules, updates, "Part 1")
+
+
+### PART 2 ###
+def compare(item1, item2):
+    if [item1, item2] in rules:
+        return -1
+    elif [item2, item2] in rules:
+        return 1
+    return 0
+
+
+def validate_rules(num: int, before: list[int], after: list[int]) -> bool:
+    for rule in rules:
+        if num in rule:
+            if num == rule[0]:
+                if rule[1] in before:
+                    return False
+            else:
+                if rule[0] in after:
+                    return False
+    return True
+
+
+bad_updates = []
+for update in updates:
+    bad = False
+    before = []
+    after = update[1:]
+    for i in range(len(update)):
+        if not validate_rules(update[i], before, after):
+            bad = True
+            break
+        before.append(update[i])
+        if len(after) > 0:
+            after.pop(0)
+    if bad:
+        bad_updates.append(update)
+
+middle_sum = 0
+for update in bad_updates:
+    update.sort(key=functools.cmp_to_key(compare))
+    middle = len(update) // 2
+    middle_sum += update[middle]
+
+print(f"part2: {middle_sum}")
