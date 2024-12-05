f20cd875ad8f29e6cd1f1a529b14d77cd893e863
johanngerberding <johann.gerberding@lector.ai>
2024-12-05 08:47:42 +0100

    day 5 part 1

---
+++ problems/day05/main.py
@@ -0,0 +1,81 @@
+test = """47|53
+97|13
+97|61
+97|47
+75|29
+61|13
+75|53
+29|13
+97|29
+53|29
+61|53
+97|53
+61|29
+47|13
+75|47
+97|75
+47|61
+75|61
+47|29
+75|13
+53|13
+
+75,47,61,53,29
+97,61,53,29,13
+75,29,13
+75,97,47,61,53
+61,13,29
+97,13,75,29,47"""
+
+test = test.split()
+rules = [[int(n) for n in el.split("|")] for el in test if "|" in el]
+updates = [[int(n) for n in el.split(",")] for el in test if "|" not in el]
+
+
+def solve(rules: list, updates: list, part: str):
+    def validate_rules(num: int, before: list[int], after: list[int]) -> bool:
+        for rule in rules:
+            if num in rule:
+                if num == rule[0]:
+                    if rule[1] in before:
+                        return False
+                else:
+                    if rule[0] in after:
+                        return False
+        return True
+
+    good_updates = []
+    for update in updates:
+        bad = False
+        before = []
+        after = update[1:]
+        for i in range(len(update)):
+            if not validate_rules(update[i], before, after):
+                bad = True
+                break
+            before.append(update[i])
+            if len(after) > 0:
+                after.pop(0)
+        if not bad:
+            good_updates.append(update)
+
+    middle_sum = 0
+    for update in good_updates:
+        middle = len(update) // 2
+        middle_sum += update[middle]
+
+    print(f"{part}: {middle_sum}")
+
+
+solve(rules, updates, "Test")
+
+
+with open("input.txt", "r") as fp:
+    data = fp.readlines()
+
+data = [el.strip() for el in data]
+data = [el for el in data if el != ""]
+rules = [[int(n) for n in el.split("|")] for el in data if "|" in el]
+updates = [[int(n) for n in el.split(",")] for el in data if "|" not in el]
+
+solve(rules, updates, "Part 1")
