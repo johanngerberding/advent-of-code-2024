1db595a2508143131888b6246564c1859e8dc3c2
johanngerberding <johann.gerberding@lector.ai>
2024-12-06 17:55:19 +0100

    some refactor, answer 2 is too high still

--- problems/day06/main.py
+++ problems/day06/main.py
@@ -15,95 +15,92 @@ inp = [el.strip() for el in inp]
 # ......#..."""
 # inp = inp.split()
 
-dirs = [(-1, 0), (0, 1), (1, 0), (0, -1)]
-curr_dir = 0
 
-for row in range(len(inp)):
-    for col in range(len(inp[row])):
-        if inp[row][col] == "^":
-            curr = (row, col, curr_dir)
-            curr_pos = (row, col)
-
-visited_with_direction = set()
-visited = set()
-# up, right, down, left
-visited.add(curr_pos)
-visited_with_direction.add(curr)
-
-# i think we have a circle if the next pos is in visited with the same direction
-# for every position you are in, you have to check where the right position of you
-# was already visited and if it has the same direction as if there was an obstacle in
-# front of you
-
-# you have to check the whole lane!! or you have to simulate further what happens until you are out
-
-obstruction_positions = 0
-
-while True:
-    next_pos = (curr[0] + dirs[curr_dir][0], curr[1] + dirs[curr_dir][1])
-    right_dir = curr_dir + 1
-    if right_dir > 3:
-        right_dir = 0
-    # move right and check if we find a position we already visited
-    # simulate the move to the right and try to find positions already visited
-    ncurr = (curr[0], curr[1])
-    while True:
-        right_pos_with_dir = (
-            ncurr[0] + dirs[right_dir][0],
-            ncurr[1] + dirs[right_dir][1],
-            right_dir,
-        )
-        if (
-            right_pos_with_dir[0] < 0
-            or right_pos_with_dir[0] >= len(inp)
-            or right_pos_with_dir[1] < 0
-            or right_pos_with_dir[1] >= len(inp[0])
-        ):
-            break
-        # if inp[right_pos_with_dir[0]][right_pos_with_dir[1]] == "#":
-        #     # change dir
-        #     right_dir += 1
-        #     if right_dir > 3:
-        #         right_dir = 0
-        #     right_pos_with_dir = (
-        #         ncurr[0] + dirs[right_dir][0],
-        #         ncurr[1] + dirs[right_dir][1],
-        #         right_dir,
-        #     )
-
-        if (
-            right_pos_with_dir[0] < 0
-            or right_pos_with_dir[0] >= len(inp)
-            or right_pos_with_dir[1] < 0
-            or right_pos_with_dir[1] >= len(inp[0])
-        ):
-            break
-
-        if right_pos_with_dir in visited_with_direction:
-            print(f"obstruction: {right_pos_with_dir}")
-            obstruction_positions += 1
-            break
-        ncurr = right_pos_with_dir
-        print(f"current position: {ncurr}")
-
-    if (
-        next_pos[0] < 0
-        or next_pos[0] >= len(inp)
-        or next_pos[1] < 0
-        or next_pos[1] >= len(inp[0])
+class Simulation:
+    def __init__(
+        self,
+        inp: list,
+        curr_dir: int = 0,
+        current: tuple | None = None,
+        visited: set | None = None,
     ):
-        # we are done
-        break
-    while inp[next_pos[0]][next_pos[1]] == "#":
-        # switch direction
-        curr_dir += 1
-        if curr_dir > 3:
-            curr_dir = 0
+        self.inp = inp
+        self.curr_dir = curr_dir
+        self.incrementers = [(-1, 0), (0, 1), (1, 0), (0, -1)]
+        if not current:
+            self._get_start()
+        else:
+            self.current = current
+        if not visited:
+            self.visited = set([self.current])
+        else:
+            self.visited = visited
 
-        next_pos = (curr[0] + dirs[curr_dir][0], curr[1] + dirs[curr_dir][1])
-    # set new position
-    curr = next_pos
-    visited.add(curr)
-    visited_with_direction.add((curr[0], curr[1], curr_dir))
+    def _get_start(self):
+        for row in range(len(self.inp)):
+            for col in range(len(self.inp[row])):
+                if self.inp[row][col] == "^":
+                    self.current = (row, col, self.curr_dir)
+                    return
 
-print(obstruction_positions)
+    def _step(self):
+        next_position = self._get_next()
+        if not self._inside(next_position):
+            # we are done
+            return False
+
+        while self.inp[next_position[0]][next_position[1]] == "#":
+            # switch direction
+            self._switch_dir()
+            next_position = self._get_next()
+
+        # set new position
+        self.current = next_position
+        self.visited.add(self.current)
+        return True
+
+    def simulate(self):
+        while self._step():
+            continue
+
+    def _switch_dir(self):
+        self.curr_dir += 1
+        if self.curr_dir > 3:
+            self.curr_dir = 0
+
+    def _inside(self, pos: tuple) -> bool:
+        return not (
+            pos[0] < 0
+            or pos[0] >= len(self.inp)
+            or pos[1] < 0
+            or pos[1] >= len(self.inp[0])
+        )
+
+    def _get_next(self) -> tuple:
+        return (
+            self.current[0] + self.incrementers[self.curr_dir][0],
+            self.current[1] + self.incrementers[self.curr_dir][1],
+        )
+
+
+part1 = Simulation(inp=inp)
+part1.simulate()
+print(f"Part 1: {len(set([(el[0], el[1]) for el in part1.visited]))}")
+
+obs = 0
+part2 = Simulation(inp=inp)
+while part2._step():
+    start = part2.current  # now I need a switch to the right
+    direction = part2.curr_dir
+    if direction > 3:
+        direction = 0
+    start = (start[0], start[1], direction)
+    visited = part2.visited
+    visited.add(start)
+    subsim = Simulation(inp=inp, curr_dir=direction, current=start, visited=visited)
+    while subsim._step():
+        if subsim.current in subsim.visited:
+            obs += 1
+            break
+
+print(obs)
