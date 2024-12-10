c008ff35236eef5b8d79899a12699e944f623805
johanngerberding <johann.gerberding@lector.ai>
2024-12-10 08:26:41 +0100

    example for part 2 works but input not

--- problems/day09/main.py
+++ problems/day09/main.py
@@ -41,11 +41,80 @@ def solve(inp: str) -> int:
     return checksum(compress(transform(inp)))
 
 
+# empty space can also be a file with value -1
+class File:
+    def __init__(self, position: int, size: int, value: int):
+        self.position = position
+        self.size = size
+        self.value = value
+        self.fixed = False
+
+    def __repr__(self) -> str:
+        return self.size * f"{self.value}"
+
+    def checksum(self):
+        return sum(
+            [i * self.value for i in range(self.position, self.position + self.size)]
+        )
+
+
+def parse(disk: str) -> list[File]:
+    parsed = []
+    num = 0
+    pos = 0
+    for i, el in enumerate(disk):
+        if i % 2 == 0:
+            parsed.append(File(pos, int(el), num))
+            num += 1
+            pos += int(el)
+        else:
+            pos += int(el)
+
+    return parsed
+
+
 result = solve(example)
 print(result)
+
+
+def solve2(inp: str):
+    parsed_files = parse(inp)
+    curr = len(parsed_files) - 1
+    for _ in range(len(parsed_files)):
+        last = parsed_files[curr]
+        if last.fixed:
+            curr -= 1
+            continue
+        found = False
+        for i in range(curr - 1):
+            diff = parsed_files[i + 1].position - (
+                parsed_files[i].position + parsed_files[i].size
+            )
+            if diff >= last.size:
+                last.position = parsed_files[i].position + parsed_files[i].size
+                parsed_files.insert(i + 1, last)
+                # remove element from old position
+                parsed_files.pop(curr + 1)
+                found = True
+                break
+        if not found:
+            curr -= 1
+        last.fixed = True
+
+    checksums = 0
+    for parsed_file in parsed_files:
+        checksums += parsed_file.checksum()
+
+    return checksums
+
+
+print(solve2(example))
 
 with open("input.txt", "r") as fp:
     inp = fp.read().strip()
 
 part1 = solve(inp)
 print(f"Part 1: {part1}")
+
+part2 = solve2(inp)
+print(f"Part 2: {part2}")
