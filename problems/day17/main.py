2012dc16550eed15daa322823e649bab41a1743a
johanngerberding <johann.gerberding@lector.ai>
2024-12-23 18:49:29 +0100

    too slow but in theory correct

--- problems/day17/main.py
+++ problems/day17/main.py
@@ -10,9 +10,11 @@ Program: 2,4,1,1,7,5,1,5,4,0,5,5,0,3,3,0"""
 def parse(inp) -> tuple:
     data = inp.split("\n\n")
     registers = [int(line.split(":")[1].strip()) for line in data[0].split("\n")]
-    program = [int(el) for el in data[1].split(":")[1].strip().split(",")]
-    program = [(program[i], program[i + 1]) for i in range(0, len(program) - 1, 2)]
-    return registers, program
+    org_program = [int(el) for el in data[1].split(":")[1].strip().split(",")]
+    program = [
+        (org_program[i], org_program[i + 1]) for i in range(0, len(org_program) - 1, 2)
+    ]
+    return registers, program, org_program
 
 
 output = []
@@ -36,44 +38,61 @@ def exec(program: list, inst_pointer: int, registers: list) -> int:
     instruction, operand = program[inst_pointer]
     if instruction == 0:
         registers[0] = registers[0] >> combo(operand, registers)
-        return inst_pointer + 1
     elif instruction == 1:
         registers[1] = registers[1] ^ operand
-        return inst_pointer + 1
     elif instruction == 2:
         registers[1] = combo(operand, registers) % 8
-        return inst_pointer + 1
     elif instruction == 3:
-        if registers[0] == 0:
-            return inst_pointer + 1
-        return operand
+        if registers[0] != 0:
+            return operand
     elif instruction == 4:
         registers[1] = registers[1] ^ registers[2]
-        return inst_pointer + 1
     elif instruction == 5:
         output.append(combo(operand, registers) % 8)
-        return inst_pointer + 1
     elif instruction == 6:
         registers[1] = registers[0] >> combo(operand, registers)
-        return inst_pointer + 1
     elif instruction == 7:
         registers[2] = registers[0] >> combo(operand, registers)
-        return inst_pointer + 1
-    raise ValueError(
-        f"This instruction {instruction} is not part of the instruction set"
-    )
+    else:
+        raise ValueError(
+            f"This instruction {instruction} is not part of the instruction set"
+        )
+    return inst_pointer + 1
 
 
-registers, program = parse(inp=example)
+registers, program, org_program = parse(inp=example)
 
 print(program)
 print(registers)
 
+output = []
+A = 0
+registers = [A, 0, 0]
 
-while instruction_pointer < len(program):
-    instruction_pointer = exec(
-        program=program, inst_pointer=instruction_pointer, registers=registers
-    )
+while True:
+    # print(f"Register A: {A}")
+    while instruction_pointer < len(program):
+        instruction_pointer = exec(
+            program=program, inst_pointer=instruction_pointer, registers=registers
+        )
+        if org_program[: len(output)] != output:
+            # print(f"problem: {program[: len(output)]} - {output}")
+            break
+
+    if org_program == output:
+        print(f"part 2 solution: {A}")
+        break
+    A += 1
+    registers = [A, 0, 0]
+    instruction_pointer = 0
+    output = []
+
+
+# while instruction_pointer < len(program):
+#     instruction_pointer = exec(
+#         program=program, inst_pointer=instruction_pointer, registers=registers
+#     )
+
 
 print(output)
 print(",".join([str(el) for el in output]))
