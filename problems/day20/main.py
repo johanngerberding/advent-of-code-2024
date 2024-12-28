0ae3941d8c49179b08d6cfede975df608b2c5be2
johanngerberding <johann.gerberding@lector.ai>
2024-12-28 10:03:46 +0100

    part 1 works but is a bit slow

--- problems/day20/main.py
+++ problems/day20/main.py
@@ -1,110 +1,15 @@
 from copy import deepcopy
 from heapq import heapify, heappop, heappush
 
-example = """###############
-#...#...#.....#
-#.#.#.#.#.###.#
-#S#...#.#.#...#
-#######.#.#.###
-#######.#.#...#
-#######.#.###.#
-###..E#...#...#
-###.#######.###
-#...###...#...#
-#.#####.#.###.#
-#.#...#.#.#...#
-#.#.#.#.#.#.###
-#...#...#...###
-###############"""
+import tqdm
 
 
-example = [[c for c in line] for line in example.split("\n")]
-print(example)
-
-graph = {}
-optionals = []
-graphs = []
-
-
-for row in range(len(example)):
-    for col in range(len(example[row])):
-        if example[row][col] == "#":
-            adjs_opt = []
-            if (
-                row > 0
-                and example[row - 1][col] in [".", "S", "E"]
-                and row < len(example) - 1
-                and example[row + 1][col] in [".", "S", "E"]
-            ):
-                adjs_opt.append((row + 1, col))
-                adjs_opt.append((row - 1, col))
-            if (
-                col < len(example[0]) - 1
-                and example[row][col + 1] in [".", "S", "E"]
-                and col > 0
-                and example[row][col - 1] in [".", "S", "E"]
-            ):
-                adjs_opt.append((row, col - 1))
-                adjs_opt.append((row, col + 1))
-
-            if len(adjs_opt) > 0:
-                optionals.append((row, col))
-
-
-org_graph = {}
-for row in range(len(example)):
-    for col in range(len(example[row])):
-        if example[row][col] == "#":
-            continue
-        if example[row][col] == "S":
-            start = (row, col)
-        if example[row][col] == "E":
-            end = (row, col)
-        adjs = []
-        if row > 0 and example[row - 1][col] in [".", "S", "E"]:
-            adjs.append((row - 1, col))
-        if row < len(example) - 1 and example[row + 1][col] in [".", "S", "E"]:
-            adjs.append((row + 1, col))
-        if col < len(example[0]) - 1 and example[row][col + 1] in [".", "S", "E"]:
-            adjs.append((row, col + 1))
-        if col > 0 and example[row][col - 1] in [".", "S", "E"]:
-            adjs.append((row, col - 1))
-
-        if len(adjs) > 0:
-            org_graph[(row, col)] = adjs
-
-
-for opt in optionals:
-    graph = {}
-    example = deepcopy(example)
-    example[opt[0]][opt[1]] = "."
-    for row in range(len(example)):
-        for col in range(len(example[row])):
-            if example[row][col] == "#":
-                continue
-            if example[row][col] == "S":
-                start = (row, col)
-            if example[row][col] == "E":
-                end = (row, col)
-            adjs = []
-            if row > 0 and example[row - 1][col] in [".", "S", "E"]:
-                adjs.append((row - 1, col))
-            if row < len(example) - 1 and example[row + 1][col] in [".", "S", "E"]:
-                adjs.append((row + 1, col))
-            if col < len(example[0]) - 1 and example[row][col + 1] in [".", "S", "E"]:
-                adjs.append((row, col + 1))
-            if col > 0 and example[row][col - 1] in [".", "S", "E"]:
-                adjs.append((row, col - 1))
-
-            if len(adjs) > 0:
-                graph[(row, col)] = adjs
-
-    graphs.append(graph)
-
-print(start)
-print(end)
-print(graph[(7, 4)])
-print(len(graph))
+def print_graph(graph):
+    g = ""
+    for line in graph:
+        g += "".join(line)
+        g += "\n"
+    print(g)
 
 
 def get_distances(graph, start):
@@ -131,12 +36,120 @@ def get_distances(graph, start):
     return distances
 
 
+# example = """###############
+# #...#...#.....#
+# #.#.#.#.#.###.#
+# #S#...#.#.#...#
+# #######.#.#.###
+# #######.#.#...#
+# #######.#.###.#
+# ###..E#...#...#
+# ###.#######.###
+# #...###...#...#
+# #.#####.#.###.#
+# #.#...#.#.#...#
+# #.#.#.#.#.#.###
+# #...#...#...###
+# ###############"""
+
+
+# example = [[c for c in line] for line in example.split("\n")]
+# print(example)
+
+with open("input.txt", "r") as fp:
+    example = [[l for l in line.strip()] for line in fp]
+
+print(example)
+graph = {}
+optionals = []
+graphs = []
+
+print("Lets find optionals")
+for row in range(len(example)):
+    for col in range(len(example[row])):
+        if example[row][col] == "#":
+            adjs_opt = []
+            if (
+                row > 0
+                and example[row - 1][col] in [".", "S", "E"]
+                and row < len(example) - 1
+                and example[row + 1][col] in [".", "S", "E"]
+            ):
+                adjs_opt.append((row + 1, col))
+                adjs_opt.append((row - 1, col))
+            if (
+                col < len(example[0]) - 1
+                and example[row][col + 1] in [".", "S", "E"]
+                and col > 0
+                and example[row][col - 1] in [".", "S", "E"]
+            ):
+                adjs_opt.append((row, col - 1))
+                adjs_opt.append((row, col + 1))
+
+            if len(adjs_opt) > 0:
+                optionals.append((row, col))
+print(f"Found {len(optionals)} optionals")
+
+print("Building the original graph...")
+org_graph = {}
+for row in range(len(example)):
+    for col in range(len(example[row])):
+        if example[row][col] == "#":
+            continue
+        if example[row][col] == "S":
+            start = (row, col)
+            example[row][col] = "."
+        if example[row][col] == "E":
+            end = (row, col)
+            example[row][col] = "."
+        adjs = []
+        if row > 0 and example[row - 1][col] in [".", "S", "E"]:
+            adjs.append((row - 1, col))
+        if row < len(example) - 1 and example[row + 1][col] in [".", "S", "E"]:
+            adjs.append((row + 1, col))
+        if col < len(example[0]) - 1 and example[row][col + 1] in [".", "S", "E"]:
+            adjs.append((row, col + 1))
+        if col > 0 and example[row][col - 1] in [".", "S", "E"]:
+            adjs.append((row, col - 1))
+
+        if len(adjs) > 0:
+            org_graph[(row, col)] = adjs
+
+print("Lets create all the optional graphs...")
+for opt in tqdm.tqdm(optionals):
+    graph = {}
+    nexample = deepcopy(example)
+    nexample[opt[0]][opt[1]] = "."
+    for row in range(len(nexample)):
+        for col in range(len(nexample[row])):
+            if nexample[row][col] == "#":
+                continue
+            adjs = []
+            if row > 0 and nexample[row - 1][col] == ".":
+                adjs.append((row - 1, col))
+            if row < len(nexample) - 1 and nexample[row + 1][col] == ".":
+                adjs.append((row + 1, col))
+            if col < len(nexample[0]) - 1 and nexample[row][col + 1] == ".":
+                adjs.append((row, col + 1))
+            if col > 0 and nexample[row][col - 1] == ".":
+                adjs.append((row, col - 1))
+
+            if len(adjs) > 0:
+                graph[(row, col)] = adjs
+
+    graphs.append(graph)
+
+print(f"Start calculating distances...")
 org_distance = get_distances(org_graph, start)[end]
 print(f"original distance: {org_distance}")
+result = 0
 for graph in graphs:
     distance = get_distances(graph, start)[end]
-    print(f"cheat distance: {distance}")
+    print(f"cheat distance: {distance} | saved {org_distance - distance}")
+    if org_distance - distance >= 100:
+        result += 1
 
+print(f"Part 1: {result}")
 # dijkstra to find the shortest path
 # incorporate the cheat -> how?
 # solve dijkstra for cheat maps, remove one tile -> run dijkstra
